9489520e9536fbcff473645cd6e90386dc02cec0
AntonSemenov <AntonSemenov@Antons-MacBook-Pro.local>
2018-08-16 09:59:49 -0500

    updates

--- main.js
+++ main.js
@@ -14,6 +14,7 @@ $(document).ready(function () {
 
     $(".animal").on("click", function () {
         var animal = $(this).attr("data-animal");
+        console.log(animal)
         var queryURL = `http://api.giphy.com/v1/gifs/search?q=${animal}&api_key=${apiKey}&limit=5`;
         $.ajax({
             url: queryURL,
@@ -21,16 +22,22 @@ $(document).ready(function () {
         }).then(function (response) {
             console.log(response.data)
         })
+        function populateButtons() {
+            for (var i = 0; i < topics.length; i++) {
+                var button = $("<button>");
+                button.text(topics[i]);
+                button.addClass("animal");
+                button.attr("data-animal", topics[i]);
+                $("#animalButtons").append(button);
+            }
+        }
     })
 
 
 });
-function populateButtons() {
-    for (var i = 0; i < topics.length; i++) {
-        var button = $("<button>");
-        button.text(topics[i]);
-        button.addClass("animal");
-        button.attr("data-animal", topics[i]);
-        $("#animalButtons").append(button);
-    }
+
+
+function showImages(response) {
+    console.log(response);
+
 }
