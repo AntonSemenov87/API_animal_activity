6c1a4c8231e8d488d4801cd8787b2224686173a1
AntonSemenov <AntonSemenov@Antons-MacBook-Pro.local>
2018-08-15 21:23:20 -0500

    first upload

---
+++ main.js
@@ -0,0 +1,36 @@
+
+var apiKey = "tCyk9O1iRm2fZbDF03zyUkK3VNun04PI";
+var topics = ["cat", "horse", "dog", "dolphin", "bird", "spider", "snake", "fox", "elephant", "owl"];
+
+$(document).ready(function () {
+    $("#addAnimal").on("click", function () {
+        var addedAnimal = $("#animal-input").val().trim();
+        topics.push(addedAnimal);
+        $("#animalButtons").empty();
+        populateButtons()
+    })
+
+    populateButtons()
+
+    $(".animal").on("click", function () {
+        var animal = $(this).attr("data-animal");
+        var queryURL = `http://api.giphy.com/v1/gifs/search?q=${animal}&api_key=${apiKey}&limit=5`;
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            console.log(response.data)
+        })
+    })
+
+
+});
+function populateButtons() {
+    for (var i = 0; i < topics.length; i++) {
+        var button = $("<button>");
+        button.text(topics[i]);
+        button.addClass("animal");
+        button.attr("data-animal", topics[i]);
+        $("#animalButtons").append(button);
+    }
+}
